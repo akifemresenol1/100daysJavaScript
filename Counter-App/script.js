24a7342a780e0d1b8bc3c216ebd8f85a00a1a5ac
Akif Emre Senol <akifffemreee@gmail.com>
2023-08-20 01:52:06 +0300

    finish counter app

--- Counter-App/script.js
+++ Counter-App/script.js
@@ -6,8 +6,26 @@ const buttons = document.querySelector(".buttons");
 
 buttons.addEventListener("click", (e) => {
   if (e.target.classList.contains("add")) {
+    counts.innerHTML++;
   }
+  if (e.target.classList.contains("subtract")) {
+    counts.innerHTML--;
+  }
+  if (e.target.classList.contains("reset")) {
+    counts.innerHTML = 0;
+  }
+  setColor();
 });
+
+function setColor() {
+  if (counts.innerHTML > 0) {
+    counts.style.color = "green";
+  } else if (counts.innerHTML < 0) {
+    counts.style.color = "orangered";
+  } else {
+    counts.style.color = "#fff";
+  }
+}
 
 // add.addEventListener("click", () => {
 //   counts.innerHTML++;
