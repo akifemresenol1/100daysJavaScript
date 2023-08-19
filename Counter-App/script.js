6f7f8ffff8e29745edc0334f4a30fe3833964bbf
Akif Emre Senol <akifffemreee@gmail.com>
2023-08-20 01:38:33 +0300

    first commit

---
+++ Counter-App/script.js
@@ -0,0 +1,22 @@
+const counts = document.querySelector(".count");
+const add = document.querySelector(".add");
+const resetCount = document.querySelector(".reset");
+const sub = document.querySelector(".subtract");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+  }
+});
+
+// add.addEventListener("click", () => {
+//   counts.innerHTML++;
+// });
+
+// sub.addEventListener("click", () => {
+//   counts.innerHTML--;
+// });
+
+// resetCount.addEventListener("click", () => {
+//   counts.innerHTML = 0;
+// });
